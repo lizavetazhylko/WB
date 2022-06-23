adf857e7dd997b1c0da7f3f53d271a24ddc3ff49
lizavetazhylko <lizavetazhylko@gmail.com>
2022-06-23 21:53:37 +0300

    Доделали карточки товара + скидки + картинки в модальном окне

--- src/js/index2.js
+++ src/js/index2.js
@@ -1,6 +1,6 @@
 import * as bootstrap from 'bootstrap';
 
-const exampleCardsWrapper = document.getElementById("wrapper");
+const exampleCardsWrapper = document.getElementById("wrapper"); // контейнер со всеми карточками
 
 fetch('https://62af8d9b3bbf46a352237ac9.mockapi.io/itemCards')
     .then((response) => response.json())
@@ -9,29 +9,54 @@ fetch('https://62af8d9b3bbf46a352237ac9.mockapi.io/itemCards')
         const fullBusket = document.getElementById('full-busket');
 
         arr.forEach(element => {
-            const cardExample = document.createElement('div');
-            const exampleCardProduct = document.createElement('div');
-            const exampleCardProductBtn = document.createElement('button');
-            const exampleCardPrice = document.createElement('span');
-            const exampleCardName = document.createElement('span');
+            const exampleCardProduct = document.createElement('div'); //див карточки
+            const exampleCardProductBtn = document.createElement('button'); // кнопка быстрого просмотра товара
+            const exampleCardInfo = document.createElement('div'); // див для цены зачеркнутой и нормальной + название товара
+            const exampleCardSaleButton = document.createElement('div'); // див для кнопки "добавить в корзину" и кружочка с % скидки
+            const exampleCardInfoWrapper = document.createElement("div");//див для цены (со скидкой) и названия товара 
+            const exampleCardPrice = document.createElement('span'); // цена
+            const exampleCardPriceSale = document.createElement('span'); // цена зачеркнутая
+            const exampleCardName = document.createElement('span'); // название товара 
+            const exampleCardProductCard = document.createElement('div');// див карточки + информации о товаре и цены
+            const exampleCardInfoSale = document.createElement("span");// кружочек с размером скидки
 
-            //cardExample.classList.add('example-card');
+
             exampleCardProduct.classList.add('example-card__product');
             exampleCardProductBtn.classList.add('example-card__product-btn');
             exampleCardPrice.classList.add('example-card__product-price');
             exampleCardName.classList.add('example-card__product-name');
+            exampleCardInfo.classList.add('example-card__info');
+            exampleCardProductCard.classList.add('example-card__product-card');
+            exampleCardInfoSale.classList.add('example-card__sale')
+            exampleCardInfoWrapper.classList.add('example-card__info-wrapper');
+            exampleCardSaleButton.classList.add('example-card__sale-button');
+            exampleCardPriceSale.classList.add('example-card__product-price-sale');
 
-            //exampleCardProduct.element.itemImg;
-            exampleCardPrice.textContent.itemPrice;
-            exampleCardName.textContent.itemName;
+            //все, что касается скидки 
+            const sale = Math.floor(Math.random() * 40);
+            console.log('sale', sale);
+            exampleCardPrice.innerHTML = element.itemPrice + " BYN";
+            exampleCardPriceSale.innerHTML = Math.round(element.itemPrice *  100 / (100 - sale)) + " BYN";
+            exampleCardName.innerHTML = element.itemName;
+            exampleCardInfoSale.innerHTML = sale + '%';
 
+            //подключение модального окна к кнопке "быстрый просмотр"
             exampleCardProductBtn.setAttribute('data-bs-toggle', 'modal');
-            exampleCardProductBtn.setAttribute('data-bs-target', '#staticBackdrop');
-            exampleCardProductBtn.append("жопа");
+            exampleCardProductBtn.setAttribute('data-bs-target', '#staticBackdrop1');
+            exampleCardProductBtn.addEventListener("click", (e) => {
+                const img2 = e.currentTarget.parentNode;
+                const divMW = document.getElementById('jopa');
+                divMW.style.backgroundImage = img2.style.backgroundImage;
+                divMW.classList.add('MW');
+            } //добавление увеличенной картинки в модальное окно
+
+            )
+            exampleCardProductBtn.append("жопа"); // кнопка быстрого просмотра
+            exampleCardProduct.style.backgroundImage = `url(${(element.itemImg)})`; //фон карточки
 
             //добавление товара в корзину 
             const exampleAddBtn = document.createElement('button');
-            exampleAddBtn.append('+');
+            exampleAddBtn.innerHTML = "+";
             exampleAddBtn.classList.add('example-card__product-add-btn')
 
             exampleAddBtn.addEventListener("click", () => {
@@ -51,8 +76,12 @@ fetch('https://62af8d9b3bbf46a352237ac9.mockapi.io/itemCards')
 
             })
 
-            exampleCardProduct.append(exampleCardProductBtn, exampleAddBtn, exampleCardPrice, exampleCardName);
-            exampleCardsWrapper.append(exampleCardProduct);
+            exampleCardSaleButton.append(exampleCardInfoSale, exampleAddBtn);
+            exampleCardInfoWrapper.append(exampleCardPrice, "   ", exampleCardName);
+            exampleCardInfo.append(exampleCardInfoWrapper, exampleCardPriceSale);
+            exampleCardProduct.append(exampleCardProductBtn, exampleCardSaleButton);
+            exampleCardProductCard.append(exampleCardProduct, exampleCardInfo);
+            exampleCardsWrapper.append(exampleCardProductCard);
         });
 
         //удаление всех товаров
